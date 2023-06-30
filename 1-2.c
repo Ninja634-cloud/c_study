8def5d4ce8627698ac1c247b2d4ae922e7e6ebdf
Ninja634-cloud <138200500+Ninja634-cloud@users.noreply.github.com>
2023-06-30 21:30:23 +0900

    Add files via upload

---
+++ 1-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void Hello() {
+    printf("Hello World\n");
+}
+
+int main() {
+    Hello();
+    return 0;
+}
+
---
+++ 1-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void a(){
+    printf("ふるいけや");
+}
+ void b(){
+    printf("かわずとびこむ");
+ }
+ void c(){
+    printf("みずのおと");
+ }
+int main(void){
+    a();
+    b();
+    c();
+    
+    return 0;
+}
